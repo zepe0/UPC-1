8f8aadbe24d7d3153a164f5043229e28280189c7
Foap2023 <Foap2023@BI101-04.epsevg.upc.es>
2024-03-05 11:25:13 +0100

    jquery repos

---
+++ GitApi/retrirepo.js
@@ -0,0 +1,12 @@
+function repo() {
+    fetch("https://api.github.com/users/octocat/repos").then((res)=>{        
+       
+      let repos = res.json()
+      var nodo = document.getElementById("repo")
+      repos.map((repo)=>{
+        nodo.innerHTML(repo.name)
+      })
+    })
+   
+
+}
